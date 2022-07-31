558add99b9c2331a18cdf00ba10ec6d630a1ba5b
Eяw!и Gontier <ierwinx@me.com>
2022-07-31 12:19:58 -0500

    Initial Commit

---
+++ AlertasView/AlertasViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertasViewApp.swift
+//  AlertasView
+//
+//  Created by Erwin Luz León on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AlertasViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
